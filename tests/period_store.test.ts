dc20149501694f1cc2e684c2c945262bf63fdf3b
p-w-g <p.grzelczyk@gmail.com>
2023-02-04 22:44:29 +0100

    todo: potential bug test

--- tests/period_store.test.ts
+++ tests/period_store.test.ts
@@ -5,3 +5,9 @@ test('render correct number of days', async ({ page }) => {
 
 	// select one month, select another month for feb it should be 28 days unless leap year
 });
+
+test('render correct day labels in schedule', async ({ page }) => {
+	await page.goto('/');
+
+	// select from a middle of the month, select like a week after; days in first column should begin at the day from
+});
