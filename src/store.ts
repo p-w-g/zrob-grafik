c85f38726885e769c09d571f5effdcb68a9b6abb
p-w-g <p.grzelczyk@gmail.com>
2023-02-04 22:40:55 +0100

    typofix

--- src/store.ts
+++ src/store.ts
@@ -5,5 +5,5 @@ export const people = writable<string[]>([]);
 export const month = writable([{}]);
 export const date_from_timestamp = writable();
 export const date_to_timestamp = writable();
-export const days_in_period = writable<Number>();
+export const days_in_period = writable<number>();
 export const nation_wide_days_off = writable();
