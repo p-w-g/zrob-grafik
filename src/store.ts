205655bf84db2dae1ccb45a74085d4ca8e676333
p-w-g <p.grzelczyk@gmail.com>
2023-02-04 22:21:36 +0100

    bootstrap shift schedule states

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,9 @@
 import { writable } from 'svelte/store';
 
 export const people = writable<string[]>([]);
+
+export const month = writable([{}]);
+export const date_from_timestamp = writable();
+export const date_to_timestamp = writable();
+export const days_in_period = writable<Number>();
+export const nation_wide_days_off = writable();
