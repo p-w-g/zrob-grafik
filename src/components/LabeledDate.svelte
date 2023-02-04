3d5fcf8c75e4bc0dd753e7ad2d4b61951e4d0d43
p-w-g <p.grzelczyk@gmail.com>
2023-02-04 22:21:59 +0100

    add missing typings, props

--- src/components/LabeledDate.svelte
+++ src/components/LabeledDate.svelte
@@ -1,6 +1,8 @@
-<script>
+<script lang="ts">
 	export let LabelText = 'Missing Label';
+	export let InputBinding: Date = new Date();
+	export let testId: string = '';
 </script>
 
 <label for="">{LabelText}</label>
-<input type="date" />
+<input type="date" data-test-id={testId} bind:value={InputBinding} />
