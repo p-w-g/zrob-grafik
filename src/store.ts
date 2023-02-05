72499071e0d7c479400fc3e9c0a1909aec9f5c54
p-w-g <p.grzelczyk@gmail.com>
2023-02-05 20:21:47 +0100

    update typing in store

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,11 @@
 import { writable } from 'svelte/store';
+import type { workday } from './typings';
 
 export const people = writable<string[]>([]);
 
 export const shifts = writable<string[]>([]);
 
-export const month = writable([{}]);
+export const month = writable<workday[]>([]);
 export const date_from_timestamp = writable();
 export const date_to_timestamp = writable();
 export const days_in_period = writable<number>();
