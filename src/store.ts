d8815798d5c4e265bcb5399d3140fae03b59299d
p-w-g <p.grzelczyk@gmail.com>
2023-02-27 17:00:06 +0100

    fix bug with wrong shift list type.

--- src/store.ts
+++ src/store.ts
@@ -3,7 +3,7 @@ import type { Ishift, workday } from './typings';
 
 export const people = writable<string[]>([]);
 
-export const shifts = writable<Ishift[]>([]);
+export const shifts = writable<string[]>([]);
 
 export const month = writable<workday[]>([]);
 export const date_from_timestamp = writable();
