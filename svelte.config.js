60f589ad99c53da3b34d4af80f00e08619fe2162
p-w-g <p.grzelczyk@gmail.com>
2023-03-09 21:23:39 +0100

    install basic netlify conf

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// unless at some point i want to have netlify edge functions.
+			// until then, its off.
+			edge: false,
+			// not sure, but i gess i dont want to split my app into functions
+			split: false
+		}
+		)
 	}
 };
 
